caef16dddfa1b949463d7e09e476712d32deed45
Deepak Gupta <34735440+Deepak172@users.noreply.github.com>
2019-02-06 08:39:52 +0530

    Add files via upload

--- bikerating/script/script.js
+++ bikerating/script/script.js
@@ -1,8 +1,9 @@
-var lastImg=2;
-document.getElementById(lastImg).className="thumb selected";
+var lastImg = 2;
+document.getElementById(lastImg).className = "thumb selected";
 
-function preview(img){
-document.getElementById(lastImg).className="thumb normal";
-img.className="thumb selected";
-document.getElementById(0).src=img.src;
-lastImg=img.id}
+function preview(img) {
+    document.getElementById(lastImg).className = "thumb normal";
+    img.className = "thumb selected";
+    document.getElementById(0).src = img.src;
+    lastImg = img.id
+}
